4e300e2c774f58d394ea714260f94fb61d238c2f
AnnyHdezSaucedo <ana_1508@hotmail.com>
2014-03-03 14:14:24 -0600

    actualizaciones de métodos new y edit
    actualizaciones de métodos new y edit en controlador, modelo y vista

--- app/controllers/clientes_controller.rb
+++ app/controllers/clientes_controller.rb
@@ -3,15 +3,25 @@ class ClientesController < ApplicationController
   end
 
   def show
+    @clientes = Cliente.find(params[:id])
   end
 
   def new
+    @clientes= Cliente.new
   end
 
   def create
+    @clientes= Cliente.new(params[:clientes])
+
+    if @clientes.save
+      redirect_to clientes_path, :notice => "Cliente guardado"
+    else
+      render "new"
+    end
   end
 
   def edit
+    @clientes= Cliente.find(params[:id])
   end
 
   def update
--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,7 +1,6 @@
 class Cliente < ActiveRecord::Base
-	attr_accessible :nombre, :a_paterno, :a_materno, :email, :t_celular, :domicilio, :fecha_alta
+ # private :nombre, :a_paterno, :a_materno, :email, :t_celular, :domicilio, :fecha_alta
 
-	validates :nombre, :a_paterno, :a_materno, :email, :t_celular, :domicilio, :fecha_alta, presence=> true
-	validates :email, :t_celular, :uniqueness => {:message => "Existe un cliente registrado con este correo "}
-
+  #validates :nombre, :a_paterno, :a_materno, :email, :t_celular, :domicilio, :fecha_alta, presence=> true
+  #validates :email, :t_celular, :uniqueness => {:message => "Existe un cliente registrado con este correo "}
 end
