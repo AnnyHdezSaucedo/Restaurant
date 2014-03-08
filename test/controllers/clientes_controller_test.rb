3fe1e92883d174f79fd8760d1a81ed38b241cba8
rocaviri <rocaviri@me.com>
2014-03-07 23:28:37 -0600

    Archivo test con estándar ruby

--- test/controllers/clientes_controller_test.rb
+++ test/controllers/clientes_controller_test.rb
@@ -1,50 +1,51 @@
-require 'test_helper'
+# para pruebas de coverage
+require 'simplecov'
+SimpleCov.start
 
+require 'test_helper'
+require 'rubocop/rake_task'
+
+# Clase de pruebas de ClientesController
 class ClientesControllerTest < ActionController::TestCase
     
     setup :initialize_cliente
-    #setup do
+    # setup do
     # @clientes = clientes(:one)
-    #end
+    # end
     
-    test "should get index" do
+    test 'should get index' do
     get :index
     assert_response :success
 end
 
-test "should get show" do
+test 'should get show' do
     get :show, id:@clientes.id
     assert_response :success
 end
 
-test "should get new" do
+test 'should get new' do
     get :new
     assert_response :success
 end
 
-test "should create client" do
+test 'should create client' do
     assert_difference('Cliente.count') do
-        post :create, :cliente => { :c_targeta_cliente => @clientes.c_targeta_cliente, :nombre => @clientes.nombre, :a_paterno => @clientes.a_paterno, :a_materno => @clientes.a_materno ,:email => @clientes.email,:t_celular => @clientes.t_celular,:domicilio => @clientes.domicilio,:fecha_alta => @clientes.fecha_alta,:c_postal => @clientes.c_postal ,:sexo => @clientes.sexo}
-        #c_targeta_cliente: '1234567', nombre: 'Jesus', a_paterno: 'Mora' a_materno: 'Rios', email:'jesus@hotmail.com',}
+        post :create, :cliente => { :c_targeta_cliente => @clientes.c_targeta_cliente, :nombre => @clientes.nombre, :a_paterno => @clientes.a_paterno, :a_materno => @clientes.a_materno, :email => @clientes.email, :t_celular => @clientes.t_celular, :domicilio => @clientes.domicilio, :fecha_alta => @clientes.fecha_alta, :c_postal => @clientes.c_postal, :sexo => @clientes.sexo }
     end
-    #assert_redirected_to @clientes
-    #assert_redirected_to clientes_path(assigns(:cliente))
-    
     assert_equal 'Client was successfully created.', flash[:notice]
 end
 
-test "should get edit" do
+test 'should get edit' do
     get :edit, id:@clientes.id
     assert_response :success
 end
 
-test "should get update" do
-    patch :update, id:@clientes.id, :cliente => { :c_targeta_cliente => @clientes.c_targeta_cliente, :nombre => @clientes.nombre, :a_paterno => @clientes.a_paterno, :a_materno => @clientes.a_materno ,:email => @clientes.email,:t_celular => @clientes.t_celular,:domicilio => @clientes.domicilio,:fecha_alta => @clientes.fecha_alta,:c_postal => @clientes.c_postal ,:sexo => @clientes.sexo}
+test 'should get update' do
+    patch :update, id:@clientes.id, :cliente => { :c_targeta_cliente => @clientes.c_targeta_cliente, :nombre => @clientes.nombre, :a_paterno => @clientes.a_paterno, :a_materno => @clientes.a_materno, :email => @clientes.email, :t_celular => @clientes.t_celular, :domicilio => @clientes.domicilio, :fecha_alta => @clientes.fecha_alta, :c_postal => @clientes.c_postal , :sexo => @clientes.sexo }
     assert_redirected_to @clientes
-    
 end
 
-test "should get destroy" do
+test 'should get destroy' do
     assert_difference('Cliente.count', -1) do
         delete :destroy, id:@clientes.id
     end
@@ -52,8 +53,8 @@ test "should get destroy" do
 end
 
 private
-def initialize_cliente
-    @clientes=clientes(:one)
-end
 
+def initialize_cliente
+    @clientes = clientes(:one)
+end
 end
